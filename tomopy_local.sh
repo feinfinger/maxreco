ac9a5dbef2c7a55fcb8a66e5c3f36766025bdb6c
fwilde <fabian.wilde@hzg.de>
2017-06-17 00:22:22 +0200

    modified python setup from istall to build option to keep things local

--- tomopy_local.sh
+++ tomopy_local.sh
@@ -1,24 +1,24 @@
 WDIR=$PWD
 git clone http://github.com/tomopy/tomopy
 cd $WDIR/tomopy
-python setup.py install --inplace
+python setup.py build
 cd $WDIR
 git clone https://github.com/scikit-image/scikit-image.git
 cd $WDIR/scikit-image
-python setupy.py install --inplace
-git clone http://github.com/feinfinger/p05tools
+python setup.py build
+git clone http://github.com/feinfinger/p05tools.git
 git clone https://github.com/hgomersall/pyFFTW.git pyfftw
 cd $WDIR/pyfftw 
 python setup.py build_ext --inplace
 cd $WDIR
 git clone http://github.com/blink1073/tifffile
 cd $WDIR/tifffile
-python setup.py install --inplace
+python setup.py build
 cd $WDIR
 git clone http://github.com/PyWavelets/pywt
 git clone http://github.com/data-exchange/DXchange dxchange
 cd $WDIR/dxchange
-python setup.py install --inplace
+python setup.py build
 cd $WDIR
 find $WDIR -type d -exec chmod 750 {} \;
 find $WDIR -type f -exec chmod 660 {} \;
