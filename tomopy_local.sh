d95ae51b86b1321cf837c6e7e4fd7850003f906b
Wilde <fwilde@win.desy.de>
2017-06-19 15:26:17 +0200

    Sorted and adapted install file.

--- tomopy_local.sh
+++ tomopy_local.sh
@@ -1,25 +1,53 @@
+#!/bin/bash
+
+# create diretories
 WDIR=$PWD
+
+# tomopy
+cd $WDIR
 git clone http://github.com/tomopy/tomopy
 cd $WDIR/tomopy
 python setup.py build
+
+# scikit-image
 cd $WDIR
 git clone https://github.com/scikit-image/scikit-image.git
 cd $WDIR/scikit-image
 python setup.py build
+cp -r scimage $WDIR
+
+# p05tools
+cd $WDIR
 git clone http://github.com/feinfinger/p05tools.git
+
+# pyfftw
+cd $WDIR
 git clone https://github.com/hgomersall/pyFFTW.git pyfftw
 cd $WDIR/pyfftw 
 python setup.py build_ext --inplace
+
+# tifffile
 cd $WDIR
 git clone http://github.com/blink1073/tifffile
 cd $WDIR/tifffile
 python setup.py build
+
+# pywt
 cd $WDIR
 git clone http://github.com/PyWavelets/pywt
-git clone http://github.com/data-exchange/DXchange dxchange
-cd $WDIR/dxchange
+cd $WDIR/pywt
 python setup.py build
+
+# dxchange
+git clone http://github.com/data-exchange/DXchange dxchange_repo
+cd $WDIR/dxchange_repo
+python setup.py build
+cp -r dxchange $WDIR
+
+# make files/dirs writeable
 cd $WDIR
 find $WDIR -type d -exec chmod 750 {} \;
 find $WDIR -type f -exec chmod 660 {} \;
+
+# finish
 echo 'TOMOPY INSTALLATION DONE.'
